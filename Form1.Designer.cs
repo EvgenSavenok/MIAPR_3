1c144a6fbc7092613e8e2ba70f00d43b3a28fa3b
EvgenSavenok <eugen.savenok@gmail.com>
2024-02-22 20:44:49 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace MIAPR_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.fakeAlertTB = new System.Windows.Forms.TextBox();
+            this.skipDetectionCB = new System.Windows.Forms.TextBox();
+            this.totalErrors = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(432, 36);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(734, 470);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Первая вероятность:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(250, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Вторая вероятность:";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(19, 84);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(245, 56);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(19, 196);
+            this.trackBar2.Maximum = 1000;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(245, 56);
+            this.trackBar2.TabIndex = 6;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(297, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Вероятность ложной тревоги:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 347);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(357, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Вероятность пропуска обнаружения:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(14, 432);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(252, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Суммарная вероятность:";
+            // 
+            // fakeAlertTB
+            // 
+            this.fakeAlertTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fakeAlertTB.Location = new System.Drawing.Point(19, 302);
+            this.fakeAlertTB.Name = "fakeAlertTB";
+            this.fakeAlertTB.Size = new System.Drawing.Size(279, 30);
+            this.fakeAlertTB.TabIndex = 10;
+            // 
+            // skipDetectionCB
+            // 
+            this.skipDetectionCB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.skipDetectionCB.Location = new System.Drawing.Point(19, 387);
+            this.skipDetectionCB.Name = "skipDetectionCB";
+            this.skipDetectionCB.Size = new System.Drawing.Size(279, 30);
+            this.skipDetectionCB.TabIndex = 11;
+            // 
+            // totalErrors
+            // 
+            this.totalErrors.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalErrors.Location = new System.Drawing.Point(19, 476);
+            this.totalErrors.Name = "totalErrors";
+            this.totalErrors.Size = new System.Drawing.Size(279, 30);
+            this.totalErrors.TabIndex = 12;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(313, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 30);
+            this.textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(313, 196);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 30);
+            this.textBox2.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1178, 543);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.totalErrors);
+            this.Controls.Add(this.skipDetectionCB);
+            this.Controls.Add(this.fakeAlertTB);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "MIAPR_3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox fakeAlertTB;
+        private System.Windows.Forms.TextBox skipDetectionCB;
+        private System.Windows.Forms.TextBox totalErrors;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace MIAPR_3
+{
+    public partial class Form1 : Form
+    {
+        private double[] firstClassPoints, secondClassPoints;
+        double mu1 = 0, mu2 = 0;
+        double sigma1 = 0, sigma2 = 0;
+        double PC1, PC2 = 0;
+        double p1 = 0, p2 = 0;
+        double falseAlarmError = 0, missingDetectingError = 0, totalClassificationError = 0;
+        double x = 0, borderX = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void InitializeMeans()
+        {
+            mu1 = 0;
+            mu2 = 0;
+            sigma1 = 0; 
+            sigma2 = 0;
+            PC1 = 0; 
+            PC2 = 0;
+            p1 = 0;
+            p2 = 0;
+            falseAlarmError = 0;
+            missingDetectingError = 0; 
+            totalClassificationError = 0;
+            x = 0; 
+            borderX = 0;
+        }
+        private void fillArrays()
+        {
+            const int pointsCount = 10000;
+            const int offset = 100;
+            firstClassPoints = new double[pointsCount];
+            secondClassPoints = new double[pointsCount];
+            Random rand = new Random();
+            for (int i = 0; i < pointsCount; i++)
+            {
+                firstClassPoints[i] = rand.Next(pictureBox.Width) - offset;
+                secondClassPoints[i] = rand.Next(pictureBox.Width) + offset;
+            }
+        }
+        private void CalculateMU()
+        {
+            for (int i = 0; i < firstClassPoints.Length; i++)
+            {
+                mu1 += firstClassPoints[i];
+                mu2 += secondClassPoints[i];
+            }
+            mu1 /= firstClassPoints.Length;
+            mu2 /= firstClassPoints.Length;
+        }
+        public void CalculateSigma()
+        {
+            for (int i = 0; i < firstClassPoints.Length; i++)
+            {
+                sigma1 += Math.Pow(firstClassPoints[i] - mu1, 2);
+                sigma2 += Math.Pow(secondClassPoints[i] - mu2, 2);
+            }
+            sigma1 = Math.Sqrt(sigma1 / firstClassPoints.Length);
+            sigma2 = Math.Sqrt(sigma2 / firstClassPoints.Length);
+        }
+        private void DrawGraphics(Graphics g, double[] points, double mu, double sigma, Color color, double PC, int scaleMode)
+        {
+            for (int x = 0; x < points.Length; x++)
+            {
+                double p = Math.Exp(-0.5 * Math.Pow((x - mu) / sigma, 2)) / (sigma * Math.Sqrt(2 * Math.PI));
+                g.FillRectangle(new SolidBrush(color), x, pictureBox.Height - (int)(p * PC * scaleMode), 3, 3);
+            }
+        }
+        private void CheckPC()
+        {
+            if (PC1 == 0)
+            {
+                falseAlarmError = 1;
+                missingDetectingError = 0;
+                totalClassificationError = 1;
+            }
+            else
+            {
+                if (PC2 == 0)
+                {
+                    falseAlarmError = 0;
+                    missingDetectingError = 0;
+                    totalClassificationError = 0;
+                }
+                else
+                {
+                    falseAlarmError /= PC1;
+                    missingDetectingError /= PC1;
+                }
+            }
+        }
+        private void RecordResults()
+        {
+            totalClassificationError = falseAlarmError + missingDetectingError;
+            fakeAlertTB.Text = falseAlarmError.ToString();
+            skipDetectionCB.Text = missingDetectingError.ToString();
+            totalErrors.Text = totalClassificationError.ToString();
+        }
+        private void CalculateErrors()
+        {
+            const int offset = 150;
+            const double EPS = 0.001;
+            x = -offset;
+            p1 = 1;
+            p2 = 0;
+            if (PC2 != 0)
+                while (p2 < p1)
+                {
+                    p1 = PC1 * Math.Exp(-0.5 * Math.Pow((x - mu1) / sigma1, 2)) /
+                        (sigma1 * Math.Sqrt(2 * Math.PI)); 
+                    p2 = PC2 * Math.Exp(-0.5 * Math.Pow((x - mu2) / sigma2, 2)) /
+                        (sigma2 * Math.Sqrt(2 * Math.PI));
+                    falseAlarmError += p2 * EPS;
+                    x += EPS;
+                }
+            borderX = x;
+            while (x < pictureBox.Width + 100)
+            {
+                p1 = Math.Exp(-0.5 * Math.Pow((x - mu1) / sigma1, 2)) /
+                    (sigma1 * Math.Sqrt(2 * Math.PI));
+                p2 = Math.Exp(-0.5 * Math.Pow((x - mu2) / sigma2, 2)) /
+                    (sigma2 * Math.Sqrt(2 * Math.PI));
+                missingDetectingError += p1 * PC1 * EPS;
+                x += EPS;
+            }
+        }
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            const int scaleMode = 150000;
+            DrawGraphics(g, firstClassPoints, mu1, sigma1, Color.Red, PC1, scaleMode);
+            DrawGraphics(g, secondClassPoints, mu2, sigma2, Color.Blue, PC2, scaleMode);
+            Pen pen = new Pen(Color.Green, 3);
+            g.DrawLine(pen, (int)borderX, 0, (int)borderX, pictureBox.Height);
+            pen.Dispose();
+        }
+        private void StartCalculations()
+        {
+            InitializeMeans();
+            PC1 = double.Parse(textBox1.Text);
+            PC2 = double.Parse(textBox2.Text);
+            fillArrays();
+            CalculateMU();
+            CalculateSigma();
+            CalculateErrors();
+            CheckPC();
+            RecordResults();
+            pictureBox.Invalidate();
+        }
+        private void UpdateTextBoxes(System.Windows.Forms.TrackBar trackBar)
+        {
+            double value1 = (double)trackBar1.Value / 1000.0;
+            double value2 = (double)trackBar2.Value / 1000.0;
+            textBox1.Text = value1.ToString("0.000");
+            textBox2.Text = value2.ToString("0.000");
+            double sum = value1 + value2;
+            if (sum > 1)
+            {
+                if (trackBar == trackBar1)
+                    trackBar2.Value = trackBar2.Maximum - trackBar1.Value;
+                else
+                    trackBar1.Value = trackBar1.Maximum - trackBar2.Value;
+                value1 = (double)trackBar1.Value / 1000.0;
+                value2 = (double)trackBar2.Value / 1000.0;
+                textBox1.Text = value1.ToString("0.000");
+                textBox2.Text = value2.ToString("0.000");
+            }
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            UpdateTextBoxes(trackBar1);
+            StartCalculations();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            UpdateTextBoxes(trackBar2);
+            StartCalculations();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            trackBar1.Value = trackBar1.Maximum / 2;
+            trackBar2.Value = trackBar2.Maximum / 2;
+            textBox1.Text = "0.5";
+            textBox2.Text = "0.5";
+            StartCalculations();
+        }
+    }
+}
